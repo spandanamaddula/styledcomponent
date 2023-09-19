b92b3fa09a4d3535cd8849747a77a54c91155405
spandana <spandanareddy2023@gmail.com>
2023-09-19 15:02:31 +0530

    StyledComponent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+
+
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React from 'react'
+
+ const StyledComponent = () => {
+    const inlineStyle={
+        backgroundColor:'orange',
+        padding:'10px',
+        borderRadius:'5px',
+    };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1 style={inlineStyle}>Inline Style Example</h1>
+        <p className="external style"> External Style Example</p>
+        <StyledComponent/>
     </div>
   );
-}
+};
+export default StyledComponent;
 
-export default App;
